1078b6fafd5bf2155d6defd279cf4d0b68dfe77a
azahar24 <azaharuddin454@gmail.com>
2022-08-21 14:04:58 +0600

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,52 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:tour_application/const/app_colors.dart';
 import 'package:tour_application/const/app_strings.dart';
 import 'package:tour_application/ui/route/route.dart';
 import 'package:tour_application/ui/views/splash_screen.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+  runApp( App());
 }
 
+class App extends StatelessWidget {
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp(
+    name: "TourApp",
+    options: FirebaseOptions(
+        apiKey: "AIzaSyBm3DBh3_8os69GQIAZWpkTBZk10olJnBo",
+        appId: "1:1052107750375:android:2b6c91771367e156f75b42",
+        messagingSenderId: "1052107750375",
+        projectId: "tourapp-d8147"),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot){
+          if(snapshot.hasError){
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if(snapshot.connectionState == ConnectionState.done){
+            return MyApp();
+          }
+          return CircularProgressIndicator();
+        }
+    );
+  }
+}
+
+
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
